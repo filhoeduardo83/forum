a056d8dd8cc0fffbefd0842813c9b4fdc9f705f5
eduardo.silvafilho <eduardo.silvafilho@dataprev.gov.br>
2020-07-04 22:09:43 -0300

    Tratando o retorno 201 no POST

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,11 +1,13 @@
 package com.example.forum.controller;
 
 import java.lang.reflect.Array;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.forum.model.Curso;
 import com.example.forum.model.Topico;
@@ -42,11 +45,14 @@ private CursoRepository cursoRepository;
 	}
 	
 	@PostMapping
-	public void addTopic(@RequestBody TopicoForm topicoForm) {
+	public ResponseEntity<TopicoDTO> addTopic(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
 		
 		Topico topico = topicoForm.converter(cursoRepository);
-		
 		topicoRepository.save(topico);
+		 //Monta a URI de resposta baseado no usuário criado
+		 URI local = uriBuilder.path("topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		 //retorna uma RespostaEntidy p retornar 201
+		 return ResponseEntity.created(local).body(new TopicoDTO(topico));
 		
 	}
 
